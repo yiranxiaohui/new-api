2752f8debaea62aa81c86cad68d2a2892f18af86
Calcium-Ion <i@caion.me>
2026-08-30 22:55:17 +0800

    Revert "fix(model): drop leftover prefill_groups unique constraints before Au…" (#7101)
    This reverts commit 69a41eeadc81adb08d04346512c76d62fd6203db.
    
    (cherry picked from commit 2bf0820f4b89530acf14d389ba3e8229211933fa)

--- model/main.go
+++ model/main.go
@@ -321,9 +321,6 @@ func migrateDB() error {
 	if err := migrateTokenModelLimitsToText(); err != nil {
 		return err
 	}
-	if err := migratePrefillGroupUniqueIndex(DB); err != nil {
-		return err
-	}
 
 	err := DB.AutoMigrate(
 		&Channel{},
@@ -386,9 +383,6 @@ func migrateDB() error {
 }
 
 func migrateDBFast() error {
-	if err := migratePrefillGroupUniqueIndex(DB); err != nil {
-		return err
-	}
 
 	var wg sync.WaitGroup
 
